fdb0d060fb5b292f1992d4aa2e06c0f663869e08
kosthon <alejofova@gmail.com>
2021-04-14 18:02:33 -0500

    Primer Avance

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+    <link rel="stylesheet" href="./public/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="icon" type="image/vnd.microsoft.icon" href="./assets/icons/ico.png" sizes="16x16 32x32">
+    <title>Gmoscape Contractor LLC  </title>
+</head>
+<body>
+    <header>
+        <section class="header">
+            <div class="header-top">
+                <div class="header-top__content container">
+                    <div class="header-logo">
+                        <img src="./assets/icons/Logo gmoscape.svg" alt="">
+                    </div>
+                    <div class="header-info">
+                        <div class="header-info__content">
+                            <span><i class="fas fa-envelope"></i> <p>manager@gmoscapect.com</p></span>
+                            <span><i class="fas fa-phone"></i> <p>(203) 343-0904</p></span>
+                        </div>
+                        <div class="header-info__social">
+                            <ul>
+                                <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                                <li><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+                                <li><a href=""><i class="fab fa-pinterest-p"></i></a></li>
+                            </ul>
+                        </div>
+                        <a class="btn-login" href="">Login</a>
+                    </div>
+                </div>
+            </div>
+            <div class="header-down">
+                <div class="header-down__content container">
+                    <nav>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Services</a></li>
+                            <li><a href="">About us</a></li>
+                            <li><a href="">Galleries</a></li>
+                            <li><a href="">Testimonials</a></li>
+                            <li><a href="">Blog</a></li>
+                            <li><a href="">Pay Here!</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            
+        </section>
+    </header>
+    <main class="containerFull">
+        <section class="hero section">
+            <div id="carouselHome" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="hero-slide">
+                            <div class="hero-slide__bg" style="background-image: url(./assets/images/banner1.jpg);"></div>
+                            <div class="hero-slide__text container">
+                                <div class="hero-slide__text-content animate__animated animate__fadeInUp">
+                                    <h2>Making your outdoor dreams a reality.</h2>
+                                    <span class="hr"></span>
+                                    <p>Gmoscape Constructor LLC</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="hero-slide">
+                            <div class="hero-slide__bg" style="background-image: url(./assets/images/banner2.jpg);"></div>
+                            <div class="hero-slide__text container">
+                                <div class="hero-slide__text-content animate__animated animate__fadeInUp">
+                                    <h2>Making your outdoor dreams a reality.</h2>
+                                    <span class="hr"></span>
+                                    <p>Gmoscape Constructor LLC</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="hero-slide">
+                            <div class="hero-slide__bg" style="background-image: url(./assets/images/Imagen\ principal.png);"></div>
+                            <div class="hero-slide__text container">
+                                <div class="hero-slide__text-content animate__animated animate__fadeInUp">
+                                    <h2>Making your outdoor dreams a reality.</h2>
+                                    <span class="hr"></span>
+                                    <p>Gmoscape Constructor LLC</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselHome" data-bs-slide="prev">
+                    <span class="btn__slider" aria-hidden="true"> <i class="fas fa-chevron-left"></i></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselHome" data-bs-slide="next">
+                    <span class="btn__slider" aria-hidden="true"> <i class="fas fa-chevron-right"></i> </span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </section>
+        <section class="resumen-main-container container section">
+            <h2>Why choose us?</h2>
+            <div class="resumen-container">         
+                <div class="resumen-text">  
+                    <div class="items__resumen">
+                        <div class="items__resumen--img">
+                            <picture>
+                                <img src="./assets/images/numero_1.png" alt="Icon number">
+                            </picture>
+                        </div>
+                        <div class="items__resumen--text">
+                            <p>
+                                At Gmoscape we work with high quality materials and the most qualified team.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="items__resumen">
+                        <div class="items__resumen--img">
+                            <picture>
+                                <img src="./assets/images/numero_2.png" alt="Icon number">
+                            </picture>
+                        </div>
+                        <div class="items__resumen--text">
+                            <p>
+                                With <b>25 years of experience</b>  we will make sure you, your family and your friends, can enjoy your outdoor living space for many years to come.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="items__resumen">
+                        <div class="items__resumen--img">
+                            <picture>
+                                <img src="./assets/images/numero_3.png" alt="Icon number">
+                            </picture>
+                        </div>
+                        <div class="items__resumen--text">
+                            <p>
+                                We have competitive prices! <b> SEE FOR YOURSELF</b>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="bbb-img-container">
+                    <picture>
+                        <img src="./assets/images/imagen_bbb.png" alt="Lobo BBB">
+                    </picture>
+                </div>
+            </div>
+            <a class="btn_orange" href="https://gmoscapect.com/pay" target="_blank">QUOTE ONLINE NOW</a>
+        </section>
+
+        
+        <section class="testimonials-main-container section">
+            <div class="banner_dream ">
+                <div class="container">
+                    <h2>BUILD YOUR DREAM PATIO WITH GMOSCAPE</h2>
+                </div>
+            </div>
+            <div class="testimonials-container container">
+                <h2>
+                    Our customers say it best!
+                </h2>
+                <div class="items-testimonials-container">
+                    <div class="testimonials__items-item">
+                            <picture>
+                                <img class="items__user-icon" src="./assets/images/icono_mujer.png" alt="Icon man">
+                            </picture>
+                        
+                        <p>
+                            Guillermo and his guys do excellent work Without a doubt one of the best hardscaper in the area I’m very very happy with my patio Thank you very much Gmoscape
+                        </p>
+                        <div class="testimonials__stars">
+                            <img src="./assets/icons/Stars.svg" alt="Icon Stars">
+                        </div>
+                        <p><strong>Crystal Harrow</strong></p>
+                    </div>
+                    <div class="testimonials__items-item">
+                            <picture>
+                                <img class="items__user-icon" src="./assets/images/Recurso_3.png" alt="Icon woman">
+                            </picture>
+                        
+                        <p>
+                            Gmoscape recently installed a patio in my backyard. It looks amazing and the whole crew was awesome! Best decision I ever made! Thanks Guillermo!
+                        </p>
+                        <div class="testimonials__stars">
+                            <img src="./assets/icons/Stars.svg" alt="Icon Stars">
+                        </div>
+                        <p><strong>Allison Seddon</strong></p>
+                    </div>
+                    <div class="testimonials__items-item">
+                        <picture>
+                            <img class="items__user-icon" src="./assets/images/icono_mujer.png" alt="Icon man">
+                        </picture>
+                        <p>
+                            I highly recommend GMOSCAPE. He’s very professional, his staff is friendly and they all do an excellent job. I am happy with the ending results of my house. Couldn’t pick any better company than this one. I am thankful. Thank you for your business and keep up the good job!
+                        </p>
+                        <div class="testimonials__stars">
+                            <img src="./assets/icons/Stars.svg" alt="Icon Stars">
+                        </div>
+                        <p><strong>Yadira Casuso</strong></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="carousel-main-container">
+            <div id="carouselHome-down" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner gallerie-home">
+                    <div class="carousel-item active">
+                        <picture>
+                            <img class="d-block w-100" src="./assets/images/fotos-3.png" alt="Image slide">
+                        </picture>
+                    </div>
+                    <div class="carousel-item">
+                        <picture>
+                            <img class="d-block w-100" src="./assets/images/fotos-4.jpg " alt="Image slide">
+                        </picture>
+                    </div>
+                    <div class="carousel-item">
+                        <picture>
+                             <img class="d-block w-100" src="./assets/images/fotos-5.jpg " alt="Image slide">
+                        </picture>
+                    </div>
+                    <div class="carousel-item">
+                        <picture>
+                            <img class="d-block w-100" src="./assets/images/fotos-2.png" alt="Image slide">
+                        </picture>
+                    </div>
+                    <div class="carousel-item">
+                        <picture>
+                            <img class="d-block w-100" src="./assets/images/fotos-1.jpg" alt="Image slide">
+                        </picture>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselHome-down" data-bs-slide="prev">
+                    <span class="btn__slider" aria-hidden="true"> <i class="fas fa-chevron-left"></i></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselHome-down" data-bs-slide="next">
+                    <span class="btn__slider" aria-hidden="true"> <i class="fas fa-chevron-right"></i> </span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </section>
+        <section class="bottom-main-container">
+            <div class="bottom-container">
+                <h3>Gmoscape Contractor LLC makes your outdoor dreams a reality</h3>
+                <p>We are experts at building landscapes and hardscapes. Whether you’re planning a new patio, poolscape, driveway, walkway, steps, or stone walls; you can rely on us to meet your expectations. Give us a call and we will guide you throughout the process, from planning to execution and building, always with excellence.</p>
+                <h3>Need More Quality Assurance?</h3>
+                <p>At Gmoscape Contractor we use pavers that meet or exceed standards set by Interlocking Concrete Pavement Institute (ICPI) and ASTM International.</p>
+                <a href="http://m.me/gmoscape" target="_blank">CHAT WITH OUR SPECIALIST</a>
+                <p class="bottom__call--number">OR CALL US NOW AT <wbr> <strong> <wbr> (203) 343-0904</strong></p>
+            </div>
+        </section>
+    </main>
+    <footer>
+    
+    </footer>
+    <script src="./public/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
+</body>
+</html>
